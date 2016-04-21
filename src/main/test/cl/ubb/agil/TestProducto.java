37f1883c58b7710788464efdc404cc3a959d85dc
MiguelCastilloVasquez <m.castillo.vas@gmail.com>
2016-04-20 20:46:06 -0400

    5.TestingProducto

--- src/main/test/cl/ubb/agil/TestProducto.java
+++ src/main/test/cl/ubb/agil/TestProducto.java
@@ -6,5 +6,111 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class TestProducto {
 	
+	@Test
+	public void TestVerificaStock(){
+		Categoria categoria = new Categoria("instrumentos","ins1");
+		Producto producto = new Producto("flauta",2,6,categoria);
+		
+		int stock = producto.getStock();
+		
+		assertThat(stock,equalTo(0));
+	}
+	
+	@Test
+	public void TestIsBajoStockTrue(){
+		Producto producto = new Producto("flauta",2,6);
+		
+		boolean stockBajo = producto.isBajoStock();
+		
+		assertThat(stockBajo,equalTo(true));
+	}
+	
+	@Test
+	public void TestIsBajoStockFalse(){
+		Existencia ex1 = new Existencia("23");
+		Existencia ex2 = new Existencia("54");
+		Producto producto = new Producto("flauta",1,6);
+		producto.addExistencia(ex1);
+		producto.addExistencia(ex2);
+		
+		boolean stockBajo = producto.isBajoStock();
+		
+		assertThat(stockBajo,equalTo(false));
+	}
+	
+	@Test
+	public void TestGetStockFaltante(){
+		Producto producto = new Producto("flauta",2,6);
+		
+		int stockFaltante = producto.getStockFaltante();
+		
+		assertThat(stockFaltante,equalTo(6));
+	}
+	
+	@Test
+	public void TestGetCategoria(){
+		Categoria categoria = new Categoria("instrumentos","ins1");
+		Producto producto = new Producto("flauta",2,6,categoria);
+		
+		Categoria cat = producto.getCategoria();
+		
+		assertThat(cat,equalTo(categoria));
+	}
+	
+	@Test
+	public void TestSetCategoria(){
+		Categoria categoria = new Categoria("instrumentos","ins1");
+		Producto producto = new Producto("flauta",2,6,categoria);
+		Categoria otra = new Categoria("limpieza","li56");
+		
+		producto.setCategoria(otra);
+		
+		assertThat(producto.getCategoria(),equalTo(otra));
+	}
+	
+	@Test
+	public void TestGetNombre(){
+		Producto producto = new Producto("flauta",2,6);
+		
+		String nombre = producto.getNombre();
+		
+		assertThat(nombre,equalTo("flauta"));
+	}
+	
+	@Test
+	public void TestSetNombre(){
+		Producto producto = new Producto("flauta",2,6);
+		producto.setNombre("guitarra");
+		
+		String nombreNuevo = producto.getNombre();
+		
+		assertThat(nombreNuevo,equalTo("guitarra"));
+	}
+	
+	@Test
+	public void TestGetProximaExistencia() throws ExcepcionDeProducto{
+		Existencia ex1 = new Existencia("23");
+		Existencia ex2 = new Existencia("54");
+		Producto producto = new Producto("flauta",1,6);
+		producto.addExistencia(ex1);
+		producto.addExistencia(ex2);
+		
+		Existencia prox = producto.getProximaExistencia();
+		
+		assertThat(prox,equalTo(ex2));
+	}
+	
+	
+	
+	@Test
+	public void TestAddExistenciaTrue(){
+		Existencia ex1 = new Existencia("23");
+		Producto producto = new Producto("flauta",1,6);
+		
+		boolean agregar = producto.addExistencia(ex1);
+		
+		assertThat(agregar,equalTo(true));
+		
+	}
 	
 }
